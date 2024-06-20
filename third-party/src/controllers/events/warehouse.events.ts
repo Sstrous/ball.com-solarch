574fb7472ac61653f3a8ed936b3826bfd5f4519f
Jelmer Verhoeven <jjp.verhoeven@student.avans.nl>
2024-06-20 12:04:32 +0200

    adjusted database connection

--- third-party/src/controllers/events/warehouse.events.ts
+++ third-party/src/controllers/events/warehouse.events.ts
@@ -1,11 +1,19 @@
-import { Event } from '../../../../libs/ball-com/export';
+import { Event, Warehouse, database } from '../../../../libs/ball-com/export';
 
 async function warehouseAddedEvent(event:Event) {
-    
+    //Add warehouse so suppliers can send products to the warehouse
+    let warehouse:Warehouse = {
+        id: event.data.id,
+        address: event.data.address
+    }
+    await database.getModel('Warehouse').create(warehouse);
 }
 
 async function warehouseUpdatedEvent(event:Event) {
-
+    let warehouse = await database.getModel('Warehouse').findOne({id: event.data.id})
+    warehouse.address = event.data.address ?? warehouse.address;
+    
+    warehouse.save();
 }
 
 export {
