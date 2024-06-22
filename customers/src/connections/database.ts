07abb73bb8580b89f4b147ab337eb6bed0ae1be4
Spike Strous <spike.strous@gmail.com>
2024-06-22 13:06:12 +0200

    Remove order functionality from customer service

--- customers/src/connections/database.ts
+++ customers/src/connections/database.ts
@@ -1,11 +1,9 @@
 import { database, EventSchema } from "../../../libs/ball-com/export";
 import CustomerSchema from "../../schemas/CustomerSchema";
-import OrdersSchema from "../../schemas/OrderSchema";
 
 function addDatabaseSchemas() {
     
     //Add schema's for reading database
-    database.getReadConnection().model('Order', OrdersSchema)
     database.getReadConnection().model('Customer', CustomerSchema)
 
     //Add event schema for writing database
--- customers/src/controllers/event.controller.ts
+++ customers/src/controllers/event.controller.ts
@@ -1,6 +1,5 @@
 import * as customerEvents from './events/customer.events';
-import * as orderEvents from './events/order.events';
-import { Order, database, Event } from "../../../libs/ball-com/export";
+import { database, Event } from "../../../libs/ball-com/export";
 
 async function customerCreatedEvent(event:Event) {
     //Update reading database
@@ -8,21 +7,6 @@ async function customerCreatedEvent(event:Event) {
     console.log("Customer created in reading database: " + event.data.email);
 }
 
-async function orderCreatedEvent(event: Event) {
-    //Create order in database
-    let order:Order = {
-        id: event.data.id,
-        customerId: event.data.customerId,
-        cancelled: false
-    }
-
-    await database.getModel('Order').create(order);
-    console.log("Order created in customer database: " + event.data.id);
-}
-
-
-
 export {
-    customerEvents,
-    orderEvents
+    customerEvents
 }
